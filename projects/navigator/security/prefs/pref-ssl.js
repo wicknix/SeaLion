9c75f12d2d957b7d76ed53d76989b0561ccb0c9c
wicknix <39230578+wicknix@users.noreply.github.com>
2023-05-19 01:53:12 -0500

    More adjustments for Mac
    Fix SSL pref pane, disable leftover sync service, and clean up mozconfig.

--- projects/navigator/security/prefs/pref-ssl.js
+++ projects/navigator/security/prefs/pref-ssl.js
@@ -7,8 +7,8 @@ function Startup()
   // map associating preference values with checkbox element IDs
   gSslPrefElements = new Map([[1, "allowTLS10"],
                               [2, "allowTLS11"],
-                              [3, "allowTLS12"]
-                              [4, "allowTKS13"]);
+                              [3, "allowTLS12"],
+                              [4, "allowTKS13"]]);
 
   // initial setting of checkboxes based on preference values
   UpdateSslBoxes();
